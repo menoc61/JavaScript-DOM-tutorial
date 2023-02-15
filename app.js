e5503d2dd8d2c5258c2c1388c1ddbe64837315f0
GILLE MOMENI <94852946+menoc61@users.noreply.github.com>
2023-02-15 10:02:38 +0100

    nodeType and nodeName

--- app.js
+++ app.js
@@ -1,29 +1,8 @@
+const banner = document.querySelector('#page-banner');
 
-const wmf = document.querySelector('#book-list li:nth-child(2) .name');
-console.log(wmf);
+console.log('#page-banner node type is:', banner.nodeType);
+console.log('#page-banner node name is:', banner.nodeName);
+console.log('#page-banner has child nodes:', banner.hasChildNodes());
 
-var books = document.querySelector('#book-list li .name');
-console.log(books);
-
-books = document.querySelectorAll('#book-list li .name');
-console.log(books);
-
-Array.from(books).forEach(function(book){
-  console.log(book);
-});
-
-
-const titles = document.getElementsByClassName('title');
-
-console.log(Array.isArray(titles));
-console.log(Array.isArray(Array.from(titles)));
-
-Array.from(titles).forEach(function(title){
-  console.log(title);
-});
-
-const search = document.getElementById('search-books');
-const bookList = document.getElementById('book-list');
-
-console.log(search, bookList);
-
+const clonedBanner = banner.cloneNode(true);
+console.log(clonedBanner);
