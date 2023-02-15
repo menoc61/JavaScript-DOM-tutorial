461b62962e7b5380e3da9118228f8a59ae4b81f6
GILLE MOMENI <94852946+menoc61@users.noreply.github.com>
2023-02-15 10:52:56 +0100

    Delete book element by condition

--- app.js
+++ app.js
@@ -1,29 +1,9 @@
+const list = document.querySelector('#book-list ul');
 
-const wmf = document.querySelector('#book-list li:nth-child(2) .name');
-console.log(wmf);
-
-var books = document.querySelector('#book-list li .name');
-console.log(books);
-
-books = document.querySelectorAll('#book-list li .name');
-console.log(books);
-
-Array.from(books).forEach(function(book){
-  console.log(book);
+// delete books
+list.addEventListener('click', (e) => {
+  if(e.target.className == 'delete'){
+    const li = e.target.parentElement;
+    li.parentNode.removeChild(li);
+  }
 });
-
-
-const titles = document.getElementsByClassName('title');
-
-console.log(Array.isArray(titles));
-console.log(Array.isArray(Array.from(titles)));
-
-Array.from(titles).forEach(function(title){
-  console.log(title);
-});
-
-const search = document.getElementById('search-books');
-const bookList = document.getElementById('book-list');
-
-console.log(search, bookList);
-
