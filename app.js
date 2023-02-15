00308c6cb31abd92d66cdbce2e160f12d566ab1a
GILLE MOMENI <94852946+menoc61@users.noreply.github.com>
2023-02-15 10:18:43 +0100

    nextElementSibling and previousElementSibling

--- app.js
+++ app.js
@@ -1,29 +1,8 @@
+const bookList = document.querySelector('#book-list');
 
-const wmf = document.querySelector('#book-list li:nth-child(2) .name');
-console.log(wmf);
+console.log('#book-list next sibling:', bookList.nextSibling);
+console.log('#book-list next element sibling:', bookList.nextElementSibling);
+console.log('#book-list previous sibling:', bookList.previousSibling);
+console.log('#book-list previous element sibling:', bookList.previousElementSibling);
 
-var books = document.querySelector('#book-list li .name');
-console.log(books);
-
-books = document.querySelectorAll('#book-list li .name');
-console.log(books);
-
-Array.from(books).forEach(function(book){
-  console.log(book);
-});
-
-
-const titles = document.getElementsByClassName('title');
-
-console.log(Array.isArray(titles));
-console.log(Array.isArray(Array.from(titles)));
-
-Array.from(titles).forEach(function(title){
-  console.log(title);
-});
-
-const search = document.getElementById('search-books');
-const bookList = document.getElementById('book-list');
-
-console.log(search, bookList);
-
+bookList.previousElementSibling.querySelector('p').innerHTML += '<br />Too cool for everyone else!';
